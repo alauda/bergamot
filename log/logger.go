5723ae1391839856f30ec3f9d9a001e6f9530f70
Daniel <daniel@alauda.io>
2017-10-24 09:23:04 +0800

    feature: adding no code file format

--- log/logger.go
+++ log/logger.go
@@ -2,6 +2,9 @@ package log
 
 import (
 	"context"
+	"fmt"
+	"os"
+	"time"
 
 	"github.com/alauda/bergamot/contexts"
 	"github.com/alauda/loggo"
@@ -77,4 +80,17 @@ func AddRequestID(ctx context.Context, fields loggo.Fields) loggo.Fields {
 // @param: size given one integer will set the size for the message on structured log
 func NewLogger(packageName string, size ...int) Logger {
 	return loggo.GetLogger(packageName, size...)
+}
+
+// NewNoCodeLogger will not print file
+func NewNoCodeLogger(name string, size ...int) Logger {
+	writter := loggo.NewSimpleWriter(os.Stdout, NoCodeFormat)
+	loggo.ReplaceDefaultWriter(writter)
+	return loggo.GetLogger(name, size...)
+}
+
+// NoCodeFormat will remove the code reference from msg
+func NoCodeFormat(entry loggo.Entry) string {
+	ts := entry.Timestamp.In(time.UTC).Format("2006-01-02 15:04:05")
+	return fmt.Sprintf("%s %s %s %s", ts, entry.Level, entry.Module, entry.Message)
 }
